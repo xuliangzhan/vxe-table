a2f189883cebb89c712714d226a973c5013ec3e6
xuliangzhan <xu_liangzhan@163.com>
2020-12-02 22:36:54 +0800

    update

--- other/src/views/Support.vue
+++ other/src/views/Support.vue
@@ -2,16 +2,26 @@
   <div class="support-page">
     <div class="support-group">
       <div class="qq-img">
-        <img src="../assets/image/support/qq.png">
+        <img v-if="disabledSupport" src="" width="190" height="190">
+        <img v-else src="../assets/image/support/qq.png">
       </div>
       <div class="qq-step">
         <ul class="step-wrapper">
-          <li>左侧扫码 或者 点击<a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=K0y8dWpsIwFtcpKfb4LlVx44bujw1FaL&jump_from=webapi"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="vxe-table 技术支持群" title="vxe-table 技术支持群"></a></li>
-          <li>下方扫码付款 ￥<span class="price-unit">￥288</span></li>
+          <li>
+            左侧扫码 或者 点击
+            <template v-if="disabledSupport">
+              <a class="is-disabled"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="vxe-table 技术支持群" title="vxe-table 技术支持群"></a>
+            </template>
+            <template v-else>
+              <a target="_blank" href="https://qm.qq.com/cgi-bin/qm/qr?k=K0y8dWpsIwFtcpKfb4LlVx44bujw1FaL&jump_from=webapi"><img border="0" src="//pub.idqqimg.com/wpa/images/group.png" alt="vxe-table 技术支持群" title="vxe-table 技术支持群"></a>
+            </template>
+          </li>
+          <li>下方扫码付款 <span class="price-unit">￥{{ disabledSupport ? '-' : supportPrice }}</span></li>
           <li>付款完成后，点击联系收款方，留言QQ号</li>
         </ul>
         <div class="btn-wrapper">
-          <button class="pay-btn" type="button" @click="payVisible = true">确认扫码付款</button>
+          <button v-if="disabledSupport" class="pay-btn is-disabled" type="button" title="当前不在支持的时间之内，工作日：9:30 ~ 18:00">确认扫码付款</button>
+          <button v-else class="pay-btn" type="button" @click="payEvent">确认扫码付款</button>
         </div>
       </div>
     </div>
@@ -25,7 +35,26 @@
 export default {
   data () {
     return {
+      supportPrice: 288,
       payVisible: false
+    }
+  },
+  computed: {
+    disabledSupport () {
+      const currDate = new Date()
+      const day = currDate.getDay()
+      const hours = currDate.getHours()
+      if (day === 0 || !((hours >= 9 && hours <= 11) || (hours >= 14 && hours <= 17) || (hours >= 19 && hours <= 21))) {
+        return true
+      }
+      return false
+    }
+  },
+  methods: {
+    payEvent () {
+      if (!this.disabledSupport) {
+        this.payVisible = true
+      }
     }
   }
 }
@@ -67,14 +96,22 @@ export default {
       vertical-align: middle;
       outline: 0;
       cursor: pointer;
-      &:hover {
-        color: #3196ff;
+      &:not(.disabled) {
+        &:hover {
+          color: #3196ff;
+        }
+        &:active {
+          border-color: #3196ff;
+        }
       }
-      &:active {
-        border-color: #3196ff;
+      &.disabled {
+        color: #BFBFBF;
       }
     }
   }
+}
+.is-disabled {
+  cursor: no-drop;
 }
 .support-pay {
   margin-top: 50px;
